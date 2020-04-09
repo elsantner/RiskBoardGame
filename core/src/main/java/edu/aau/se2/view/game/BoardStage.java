9390e88deaab9f3b860e5078475e68b7e9609bd3
Elias <elias.santner@edu.htl-villach.at>
2020-04-10 00:40:51 +0200

    implement camera movement and improved zoom
    The zoom is now bounded and a double tap can be used to quickly zoom in and out.

--- core/src/main/java/edu/aau/se2/view/game/BoardStage.java
+++ core/src/main/java/edu/aau/se2/view/game/BoardStage.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -13,20 +14,25 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
  * @author Elias
  */
 public class BoardStage extends Stage implements GestureDetector.GestureListener {
+    private static float MAX_ZOOM_FACTOR = 1, MIN_ZOOM_FACTOR = 0.25f;
+
     private Texture texRiskBoard;
     private Image imgRiskBoard;
+    private OrthographicCamera cam;
 
     private float prevZoomFactor = 1;
+    private long prevTapTime = 0;
 
     public BoardStage() {
         super(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        cam = (OrthographicCamera) this.getCamera();
         loadAssets();
         setupBoardImage();
     }
 
     private void loadAssets() {
         texRiskBoard = new Texture("riskBoard.jpg");
-        imgRiskBoard= new Image(texRiskBoard);
+        imgRiskBoard = new Image(texRiskBoard);
     }
 
     private void setupBoardImage() {
@@ -37,14 +43,51 @@ public class BoardStage extends Stage implements GestureDetector.GestureListener
     }
 
     public float getZoomFactor() {
-        return ((OrthographicCamera) this.getCamera()).zoom;
+        return cam.zoom;
     }
 
     public void setZoomFactor(float zoomFactor) {
         if (zoomFactor > 1 || zoomFactor < 0) {
             throw new IllegalArgumentException("zoomFactor must be between 0 and 1");
         }
-        ((OrthographicCamera) this.getCamera()).zoom = zoomFactor;
+        cam.zoom = zoomFactor;
+    }
+
+    /**
+     * Moves the camera position by deltaX and deltaY while staying inside the bounds of the camera viewport and considering zoom factor.
+     * @param deltaX The change of the x position
+     * @param deltaY The change of the x position
+     */
+    private void moveCameraWithinBoardBounds(float deltaX, float deltaY) {
+        // zoom factor is incorporated to make (absolute) camera movement slower when zoomed in so it's always the same visual velocity
+        float newX = cam.position.x - deltaX*getZoomFactor();
+        float newY = cam.position.y + deltaY*getZoomFactor();
+
+        // the camera position is the center coordinate of the displayed area --> cam.viewport.../2
+        cam.position.x = MathUtils.clamp(newX, cam.viewportWidth/2 - (cam.viewportWidth/2)*(1-getZoomFactor()),
+                cam.viewportWidth/2 + (cam.viewportWidth/2)*(1-getZoomFactor()));
+        cam.position.y = MathUtils.clamp(newY, cam.viewportHeight/2 - (cam.viewportHeight/2)*(1-getZoomFactor()),
+                cam.viewportHeight/2 + (cam.viewportHeight/2)*(1-getZoomFactor()));
+        cam.update();
+    }
+
+    /**
+     * Zooms the camera if a double tap happened (2 taps within 1 second).
+     */
+    private void zoomOnDoubleTap() {
+        if (System.currentTimeMillis() - prevTapTime < 1000) {
+            prevTapTime = 0;
+            if (getZoomFactor() == MAX_ZOOM_FACTOR) {
+                setZoomFactor(MIN_ZOOM_FACTOR);
+            }
+            else {
+                setZoomFactor(MAX_ZOOM_FACTOR);
+            }
+            moveCameraWithinBoardBounds(0, 0);
+        }
+        else {
+            prevTapTime = System.currentTimeMillis();
+        }
     }
 
     @Override
@@ -56,6 +99,7 @@ public class BoardStage extends Stage implements GestureDetector.GestureListener
 
     @Override
     public boolean tap(float x, float y, int count, int button) {
+        zoomOnDoubleTap();
         return false;
     }
 
@@ -71,6 +115,7 @@ public class BoardStage extends Stage implements GestureDetector.GestureListener
 
     @Override
     public boolean pan(float x, float y, float deltaX, float deltaY) {
+        moveCameraWithinBoardBounds(deltaX, deltaY);
         return false;
     }
 
@@ -82,9 +127,10 @@ public class BoardStage extends Stage implements GestureDetector.GestureListener
     @Override
     public boolean zoom(float initialDistance, float distance) {
         float newZoomFactor = prevZoomFactor * initialDistance/distance;
-        if (newZoomFactor <= 1) {
+        if (newZoomFactor <= MAX_ZOOM_FACTOR && newZoomFactor >= MIN_ZOOM_FACTOR) {
             // set zoom, but consider the zoom factor where the last gesture ended
             setZoomFactor(newZoomFactor);
+            moveCameraWithinBoardBounds(0, 0);
         }
         return false;
     }
