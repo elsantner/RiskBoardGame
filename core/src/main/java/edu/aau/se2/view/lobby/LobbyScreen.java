37f0d59a0285f7dc64a86eecf617a231713837da
willistpf <willi.stuhle@gmail.com>
2020-04-18 17:41:01 +0200

    Improved LobbyScreen to fit various device resolutions

--- core/src/main/java/edu/aau/se2/view/lobby/LobbyScreen.java
+++ core/src/main/java/edu/aau/se2/view/lobby/LobbyScreen.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.*;
 
 
 import java.util.ArrayList;
@@ -25,6 +28,8 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
     private Texture line;
     private SpriteBatch batch;
     private BitmapFont font;
+    private int height;
+    private int width;
 
     private Database db;
     private List<Player> users;
@@ -32,8 +37,20 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
     public LobbyScreen() {
         db = Database.getInstance();
         db.setPlayersChangedListener(this);
-        assets();
+
         users = new ArrayList<>();
+
+        height = Gdx.graphics.getHeight();
+        width = Gdx.graphics.getWidth();
+        // if for some reason phone screen is flipped
+        if (height > width) {
+            int tmp = height;
+            height = width;
+            width = tmp;
+        }
+        batch = new SpriteBatch();
+
+        assets();
     }
 
     @Override
@@ -47,13 +64,13 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch = new SpriteBatch();
         batch.begin();
 
         batch.draw(background, 0, 0);
         batch.draw(lobbyOverlay, 0, 0);
-        batch.draw(lobbyText, 75, 800);
-        batch.draw(line, 50, 785);
+        batch.draw(line, 0, 0);
+        batch.draw(lobbyText, 0, 0);
+
 
         batch.end();
         renderUsers();
@@ -61,8 +78,8 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
 
     private void renderUsers() {
 
-        int xCord = 85;
-        int yCord = 775;
+        int xCord = (int)((width * 55) / 1080);
+        int yCord = (int)((height  * 760) / 1080);
 
         batch.begin();
         for (Player us : users
@@ -73,12 +90,12 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
             font.draw(batch, name, xCord, yCord);
             if (ready) {
                 font.setColor(new Color(0, 0.8f, 0, 1));
-                font.draw(batch, "ready", (xCord + 1100), yCord);
+                font.draw(batch, "ready", (xCord + (int)((width * 1200)/ 1920)), yCord);
             } else {
                 font.setColor(new Color(0.8f, 0, 0, 1));
-                font.draw(batch, "!ready", (xCord + 1100), yCord);
+                font.draw(batch, "!ready", (xCord +(int)((width * 1200)/ 1920)), yCord);
             }
-            yCord -= 150;
+            yCord -= (int)((height * 150) / 1080);
         }
         batch.end();
 
@@ -114,14 +131,31 @@ public class LobbyScreen implements Screen, OnPlayersChangedListener {
     }
 
     private void assets() {
-        Gdx.app.log(TAG, "Loading assets");
-        font = new BitmapFont(Gdx.files.internal("font/lobbyFontv2.fnt"));
-        font.getData().scale(0.05f);
+        Gdx.app.log(TAG, "Loading assets" + Gdx.graphics.getDensity() + "  "+ height);
+
+        background = scaleToScreen("lobby/lobbyScreen.png");
+        lobbyText = scaleToScreen("lobby/lobby2.png");
+        line = scaleToScreen("lobby/line.png");
+        lobbyOverlay = scaleToScreen("lobby/lobbyMenuOverlay.png");
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/CenturyGothic.ttf"));
+        FreeTypeFontGenerator.setMaxTextureSize(5000);
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = (height * 150) / 1080;
+        parameter.borderColor = Color.BLACK;
+        parameter.borderWidth = (height * 4) / 1080f;
+        font = generator.generateFont(parameter);
         font.setColor(new Color(0.6f, 0, 0, 1));
-        background = new Texture(Gdx.files.internal("lobby/lobbyScreen.png"));
-        lobbyText = new Texture(Gdx.files.internal("lobby/lobby2.png"));
-        lobbyOverlay = new Texture(Gdx.files.internal("lobby/lobbyMenuOverlay.png"));
-        line = new Texture(Gdx.files.internal("lobby/line.png"));
+        generator.dispose();
+
+    }
+
+    private Texture scaleToScreen(String path) {
+        Pixmap pixmap = new Pixmap(Gdx.files.internal(path));
+
+        Pixmap pixmap1 = new Pixmap(width, height, pixmap.getFormat());
+        pixmap1.drawPixmap(pixmap, 0, 0, pixmap.getWidth(), pixmap.getHeight(), 0, 0, pixmap1.getWidth(), pixmap1.getHeight());
+        return new Texture(pixmap1);
     }
 
     public List<Player> getUsers() {
