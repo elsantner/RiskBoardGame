7955237802b39d1dce504ea5c14afc99a30c3a3b
klealu <61950349+klealu@users.noreply.github.com>
2020-04-20 22:57:11 +0200

    Modified the Main Menu Class (several aspects are modified, the representation of the button on the code, the functionality of the screen, etc.)

--- core/src/main/java/edu/aau/se2/MainMenu.java
+++ core/src/main/java/edu/aau/se2/MainMenu.java
@@ -3,50 +3,62 @@ package edu.aau.se2;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenu implements Screen {
-    private RiskGame parent;
+    private RiskGame riskGame;
+    private Texture texture;
+    private Skin mySkin;
     private Stage stage;
-    private Table table;
-    private Skin skin;
-    private TextButton createGame;
-    private TextButton joinGame;
-    private TextButton settings;
-    private TextButton exit;
+    private Viewport gamePort;
+    private Label gameTitle;
+    private Button create;
+    private Button join;
+    private Button settings;
+    private Button exit;
+
 
     public MainMenu(RiskGame riskGame){
-        parent = riskGame; //allows the screen to tell the orchestrator when done
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
+        this.riskGame = riskGame;
+        mySkin = new Skin(Gdx.files.internal("skin/star-soldier-ui.json"));
+        gamePort = new ScreenViewport();
+        stage = new Stage(gamePort);
+        gameTitle = new Label("GAME MENU", mySkin);
+        gameTitle.setSize(GameConstants.col_width*2, GameConstants.row_height*2);
+        gameTitle.setPosition(GameConstants.cX - gameTitle.getWidth()/2, GameConstants.cY + GameConstants.row_height);
+        gameTitle.setAlignment(Align.center);
 
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
-        stage.draw();
+        create = new TextButton("Create Game", mySkin);
+        create.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        create.setPosition(GameConstants.cX - create.getWidth()/2, GameConstants.cY + GameConstants.row_height);
 
-        //create the the table that contains the buttons
-        table = new Table();
-        table.setFillParent(true);
-        table.setDebug(true);
-        table.addActor(table);
+        join = new TextButton("Join Game", mySkin);
+        join.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        join.setPosition(GameConstants.cX - join.getWidth()/2, create.getY() - GameConstants.row_height);
 
-        skin = new Skin(Gdx.files.internal("skin/star-soldier-ui.json"));
-        createGame = new TextButton("Create Game", skin);
-        joinGame = new TextButton("Join Game", skin);
-        settings = new TextButton("Settings", skin);
-        exit = new TextButton("Exit", skin);
+        settings = new TextButton("Settings", mySkin);
+        settings.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        settings.setPosition(GameConstants.cX - settings.getWidth()/2, join.getY() - GameConstants.row_height);
 
-        //add the buttons to the table
-        table.add(createGame).fillX().uniformX();
-        table.row().pad(10,0,10,0);
-        table.add(joinGame).fillX().uniformX();
-        table.row();
-        table.add(settings).fillX().uniformX();
-        table.row();
-        table.add(exit).fillX().uniformX();
+        exit = new TextButton("Exit Game", mySkin);
+        exit.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        exit.setPosition(GameConstants.cX - exit.getWidth()/2, settings.getY() - GameConstants.row_height);
+
+
+        stage.addActor(gameTitle);
+        stage.addActor(create);
+        stage.addActor(join);
+        stage.addActor(settings);
+        stage.addActor(exit);
 
     }
 
@@ -58,18 +70,16 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        //clear the screen before drawing the next screen
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClearColor(1,0,0,0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.act();
         stage.draw();
 
     }
 
     @Override
     public void resize(int width, int height) {
-        //automatically recenter when changing elements on the stage
-        stage.getViewport().update(width, height, true);
+        gamePort.update(width, height);
     }
 
     @Override
@@ -89,6 +99,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
-
+        mySkin.dispose();
+        stage.dispose();
     }
 }
