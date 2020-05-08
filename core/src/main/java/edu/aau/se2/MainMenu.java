dc86380fd84b6c8674d8204ceac9718ac70d4460
klealu <61950349+klealu@users.noreply.github.com>
2020-05-08 17:58:22 +0200

    Added the background image.

--- core/src/main/java/edu/aau/se2/MainMenu.java
+++ core/src/main/java/edu/aau/se2/MainMenu.java
@@ -5,16 +5,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -22,48 +17,39 @@ import edu.aau.se2.model.Database;
 
 public class MainMenu implements Screen {
     private RiskGame riskGame;
-    private Texture texture;
     private Skin mySkin;
     private Stage stage;
     private Viewport gamePort;
-    private Label gameTitle;
     private Button create;
     private Button join;
-    private Button settings;
     private Button exit;
-
+    private Texture backgroundTxt;
 
     public MainMenu(RiskGame riskGame){
         this.riskGame = riskGame;
-        mySkin = new Skin(Gdx.files.internal("skinMainMenu/star-soldier-ui.json"));
+        mySkin = new Skin(Gdx.files.internal("skinMainMenu/glassy-ui.json"));
         gamePort = new ScreenViewport();
         stage = new Stage(gamePort);
-        gameTitle = new Label("GAME MENU", mySkin);
-        gameTitle.setSize(GameConstants.col_width*2, GameConstants.row_height*2);
-        gameTitle.setPosition(GameConstants.cX - gameTitle.getWidth()/2, GameConstants.cY + GameConstants.row_height);
-        gameTitle.setAlignment(Align.center);
 
         create = new TextButton("Create Game", mySkin);
-        create.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        create.setTransform(true);
+        create.setScale(1f);
         create.setPosition(GameConstants.cX - create.getWidth()/2, GameConstants.cY + GameConstants.row_height);
 
         join = new TextButton("Join Game", mySkin);
-        join.setSize(GameConstants.col_width*2, GameConstants.row_height);
+        join.setTransform(true);
+        join.setScale(1f);
         join.setPosition(GameConstants.cX - join.getWidth()/2, create.getY() - GameConstants.row_height);
 
-        settings = new TextButton("Settings", mySkin);
-        settings.setSize(GameConstants.col_width*2, GameConstants.row_height);
-        settings.setPosition(GameConstants.cX - settings.getWidth()/2, join.getY() - GameConstants.row_height);
-
         exit = new TextButton("Exit Game", mySkin);
-        exit.setSize(GameConstants.col_width*2, GameConstants.row_height);
-        exit.setPosition(GameConstants.cX - exit.getWidth()/2, settings.getY() - GameConstants.row_height);
+        exit.setTransform(true);
+        exit.setScale(1f);
+        exit.setPosition(GameConstants.cX - exit.getWidth()/2, join.getY() - GameConstants.row_height);
 
+        backgroundTxt = new Texture(Gdx.files.internal("lobby/lobbyScreen.png"));
 
-        stage.addActor(gameTitle);
         stage.addActor(create);
         stage.addActor(join);
-        stage.addActor(settings);
         stage.addActor(exit);
 
         onClickButtons();
@@ -86,12 +72,6 @@ public class MainMenu implements Screen {
             }
         });
 
-        settings.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                //ToDo Implement the Settings Screen
-            }
-        });
 
         exit.addListener(new ChangeListener() {
             @Override
@@ -104,16 +84,21 @@ public class MainMenu implements Screen {
     }
 
 
-
     @Override
     public void show() {
-
     }
 
     @Override
     public void render(float delta) {
+        stage.act(Gdx.graphics.getDeltaTime());
         Gdx.gl.glClearColor(1,0,0,0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        stage.getBatch().begin();
+        stage.getBatch().draw(backgroundTxt,0,0, stage.getViewport().getScreenWidth(), stage.getViewport().getScreenHeight());
+        stage.getBatch().end();
+
         stage.act();
         stage.draw();
 
@@ -121,7 +106,8 @@ public class MainMenu implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        gamePort.update(width, height);
+        gamePort.update(width, height, true);
+        stage.getViewport().update(width, height);
     }
 
     @Override
@@ -141,6 +127,8 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
+        stage.getBatch().dispose();
+        backgroundTxt.dispose();
         mySkin.dispose();
         stage.dispose();
     }
