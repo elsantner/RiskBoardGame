c5163a37f041957783b343f2eb540557c3603e70
Elias <elias.santner@edu.htl-villach.at>
2020-05-17 20:13:57 +0200

    Improvements to code quality (sonarcloud) #2

--- core/src/main/java/edu/aau/se2/view/game/CardStage.java
+++ core/src/main/java/edu/aau/se2/view/game/CardStage.java
@@ -11,17 +11,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import edu.aau.se2.model.listener.OnCardsChangedListener;
-import edu.aau.se2.utils.LoggerConfigurator;
 import edu.aau.se2.view.AbstractScreen;
 import edu.aau.se2.view.AbstractStage;
 
 public class CardStage extends AbstractStage implements OnCardsChangedListener {
-
-    private static final String TAG = "CardStage";
+    
     private boolean updated;
 
     private Table cardContainer;
